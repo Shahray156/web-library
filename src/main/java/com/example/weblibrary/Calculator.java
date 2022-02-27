695faf2b0d4030e2484fa9dad8070ef33a88e4b0
Shahray156 <shahray1561@gmail.com>
2022-02-27 22:49:16 +0300

    commit

---
+++ src/main/java/com/example/weblibrary/Calculator.java
@@ -0,0 +1,8 @@
+package com.example.weblibrary;
+
+public interface Calculator {
+    int plus(int num1, int num2);
+    int minus(int num1, int num2);
+    int multiply(int num1, int num2);
+    int divide(int num1, int num2);
+}
